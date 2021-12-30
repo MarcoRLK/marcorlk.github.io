cc4fb64fde8db71e241e610920041ea4e06f03e8
Marco Kuroiva <marco.antoniorl10@gmail.com>
2021-12-30 11:41:44 -0300

    Changing Panel Cards and adding animated icons

---
+++ src/components/AnimatedIcon.js
@@ -0,0 +1,30 @@
+import React, { Component, useState} from 'react';
+import Lottie from 'lottie-react-web'
+ 
+const Icon = (props) => {
+    const [isStopped, setIsStopped] = useState(false)
+
+    const animationOptions = {
+        loop: false,
+        autoplay: false,
+        animationData: props.icon,
+    }
+
+    return (
+    <div onMouseEnter={() => setIsStopped(false)}
+         onMouseLeave={() => setIsStopped(true)}>
+        <Lottie options = {animationOptions} height={100} width={100} isStopped={isStopped}>
+        </Lottie>
+    </div>
+
+
+
+    )
+
+
+
+} 
+
+
+ 
+export default Icon
--- src/components/Panels.js
+++ src/components/Panels.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React  from 'react'
 import './Panels.css'
-import SkillCard from './SkillCard'
+import { CardGroup, Container, Card} from 'react-bootstrap'
+import AnimatedIcon from './AnimatedIcon';
+import Icon1 from './680-it-developer-outline-edited.json'
+import Icon2 from './153-bar-chart-growth-outline-edited.json'
+import Icon3 from './112-book-morph-outline-edited.json'
 
 function Panels() {
+
     return (
         <div className='card-box'>
             <div className='title-reasons'>
@@ -10,47 +15,67 @@ function Panels() {
             </div>
 
             <div className='reasons'>
-                <div className='single-reason'>
-                    <SkillCard title="🧑‍💻 Boa base de programação"
-                                image="https://wp-media.petersons.com/blog/wp-content/uploads/2017/12/10124351/luca-bravo-217276-unsplash.jpg"
-                                item1="Bacharel em Engenharia de Software pela Universidade de Brasília (2/2019)"
-                                item2="Aluno destaque dentre os formandos"
-                                item3="Principal linguagem: Python"
-                                item4="Familiaridade com: Java, C, R, Javascript"
-                                link1="Github"
-                                link2="Exemplo de projeto"
-                    />
-                </div>
-                <div className='single-reason'>
-                    <SkillCard title="🎲 Experiência com Banco de Dados, manipulação de dados e Machine Learning"
-                                image="https://cdn.corporatefinanceinstitute.com/assets/database-1024x703.jpeg"
-                                item1="Estágio no Tribunal de Contas da União (TCU). Trabalhos com Oracle, SQL Server e R"
-                                item2="TCC sobre técnicas de ML e PLN na identificação de comentários impróprios na web"
-                                item3="Familiaridade com: scikit learn, pandas, numpy"
-                                link1="Certificado: aprendizagem supervisionada"
-                                link2="Certificado: aprendizagem não-supervisionada"
-                    />
-                </div>
-                <div className='single-reason'>
-                    <SkillCard title="🧮 Boa base de Probabilidade e Estatística"
-                                image="https://cdn.analyticsvidhya.com/wp-content/uploads/2019/09/introduction-to-probability-and-statistics-for-epidemiology_HRP259.jpg"
-                                item1="Cursos regulares da Engenharia como: Cálculo 1 e 2, Métodos Numéricos, Probabilidade e Estatística, Álgebra Linear, etc"
-                                item2="Conhecimento sobre algoritmos comumente usados em ML como: Naive Bayes, Árvore de Decisão, Regressão Linear, entre outros"
-                                link1="Meu artigo 1"
-                                link2="Meu artigo 2"
-                    />
-                </div>
-                <div className='single-reason'>
-                <SkillCard title="⭐ Diversos"
-                                image="https://venturebeat.com/wp-content/uploads/2021/05/GettyImages-947663966-e1621956778415.jpg?fit=750%2C380&strip=all"
-                                item1="Inglês fluente"
-                                item2="Familiaridade com diversas metodologias de desenvolvimento (ágeis, RUP, etc)"
-                                item3="Testes (Pytest)"
-                                item4="DevOps (Docker)"
-                                link1="Documentação CRP"
-                                link2="Documentação Drogaria Ideal"
-                    />
-                </div>
+                <Container>
+                    <CardGroup>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Body>
+                                <div className='icon' >
+                                    <AnimatedIcon icon={Icon1}/>
+                                </div>                                
+                                <Card.Title>Engenheiro de Software</Card.Title>
+                                <Card.Text>
+                                    Durante a minha graduação, tive a oportunidade de trabalhar com diversas linguagens, metodologias de desenvolvimento e tecnologias
+                                </Card.Text>
+                                <Card.Subtitle>Principal Linguagem</Card.Subtitle>
+                                <Card.Text>
+                                    Python
+                                <Card.Subtitle>Outras tecnologias</Card.Subtitle>                    
+                                    Java, C, R e Javascript
+                                <Card.Subtitle>Metodologias de desenvolvimento</Card.Subtitle>
+                                    Ágil, RUP, cascada, etc.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Body>
+                                <div className='icon' >
+                                    <AnimatedIcon icon={Icon2}/>
+                                </div>   
+                                <Card.Title>Interessado em Dados</Card.Title>
+                                <Card.Text>
+                                    Desde que fiz estágio no Tribunal de Contas da União (TCU), onde tive a oportunidade de trabalhar com Oracle, SQL Server e R, sou bastante interessado em projetos que envolvam a manipulação de dados e Machine Learning.
+                                </Card.Text>
+                                <Card.Subtitle>Exemplo de projeto</Card.Subtitle>
+                                <Card.Text>
+                                    TCC sobre técnicas de ML e PLN na identificação de comentários impróprios na web
+                                <Card.Subtitle>Bibliotecas e SGBDs</Card.Subtitle>                    
+                                    scikit learn, pandas, numpy, Oracle, SQL Server e PostgreSQL
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Body>
+                                <div className='icon' >
+                                    <AnimatedIcon icon={Icon3}/>
+                                </div> 
+                                <Card.Title>Diversos</Card.Title>
+                                <Card.Text>
+                                    O curso de ESW me permitiu adquirir familiaridade com diversas metodologias de desenvolvimento (ágeis, RUP, etc)
+                                </Card.Text>
+                                Conhecimento sobre algoritmos de ML como: Naive Bayes, Árvore de Decisão, Regressão Linear, entre outros
+                                <Card.Subtitle>Idiomas:</Card.Subtitle>
+                                <Card.Text>
+                                Português e Inglês
+                                <Card.Subtitle>Outras tecnologias:</Card.Subtitle>                    
+                                    Testes (Pyteste)
+                                    DevOps (Docker)
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </CardGroup>
+                </Container>
             </div>
         </div>
     )
